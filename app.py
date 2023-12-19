920c00f9377328818950206d4d6970973b9c8939
Ryan Beck <ryanbeck@Jasmines-MacBook-Pro.local>
2023-12-19 15:48:26 -0500

    rake through description

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
+from rake_nltk import Rake
+from keybert import KeyBERT
 import streamlit as st
 import re
 from pdfminer.high_level import extract_text
 
-st.title("Milky Way Matchmaking")
-uploaded_file = st.file_uploader("Upload your resume here")
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+stopWords = stopwords.words('english')
+LEMMATIZER = WordNetLemmatizer()
+
+# ----------- Functions -------------
+
+
+def normalized_text(text: str):
+    text = text.lower()
+    text = text.strip()
+    text = re.sub('[^\w\s]', '', text)
+    text = text.split()
+    text = [word for word in text if word.isalpha()]
+    text = [word for word in text if word not in stopWords and len(word) >= 2]
+    text = [LEMMATIZER.lemmatize(word) for word in text]
+    return ' '.join(map(str, text))
 
 
 def extract_text_from_pdf(file):
@@ -40,10 +64,6 @@ def extract_name_from_resume(text):
     return name
 
 
-skills_list = ['Python', 'Data Analysis', 'Machine Learning',
-               'Communication', 'Project Management', 'Deep Learning', 'SQL', 'Tableau']
-
-
 def extract_skills_from_resume(text, skills_list):
     skills = []
     for skill in skills_list:
@@ -52,6 +72,16 @@ def extract_skills_from_resume(text, skills_list):
         if match:
             skills.append(skill)
     return skills
+
+
+# --------------- Stream-lit Display -----------
+
+st.title("Milky Way Matchmaking")
+uploaded_file = st.file_uploader("Upload your resume here")
+
+
+skills_list = ['Python', 'Data Analysis', 'Machine Learning',
+               'Communication', 'Project Management', 'Deep Learning', 'SQL', 'Tableau']
 
 
 if uploaded_file:
@@ -73,4 +103,22 @@ text_input = st.text_input(
 )
 
 if text_input:
-    st.write("You entered: ", text_input)
+    # st.write("You entered: ", text_input)
+    # kw_model = KeyBERT()
+    # keywords = kw_model.extract_keywords(text_input)
+    # print(keywords)
+    #
+    normText = normalized_text(text_input)
+    print(normText)
+    # rake_nltk_var = Rake()
+    # rake_nltk_var.extract_keywords_from_text(text_input)
+    # keyword_extracted = rake_nltk_var.get_ranked_phrases()
+    # print(keyword_extracted)
+    r = Rake()
+    a = r.extract_keywords_from_text(text_input)
+    b = r.get_ranked_phrases()
+    c = r.get_ranked_phrases_with_scores()
+    # e = r.extract_keywords_from_sentences(normText)
+    # print(b)
+    print(c)
+    # print(e)
